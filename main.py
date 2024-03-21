c62ad3197945ccb65cb8a0e585c539552479a72e
JakubKaczmarek5764 <247686@edu.p.lodz.pl>
2024-03-21 18:15:47 +0100

    first

---
+++ main.py
@@ -0,0 +1,10 @@
+
+class Graph:
+    # nie robic klasy noda, zrobic metody LRUD w grafie i tyle
+    def __init__(self, current, goal):
+        self.current = current
+        self.goal = goal
+
+b1 = [[0,1,2,3], [4,5,6,7], [8,9,10,11], [12,13,14,15]]
+b2 = [[0,1,2,3], [4,5,6,7], [8,9,10,11], [12,13,14,15]]
+print(b1)
